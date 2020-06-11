cf8f60d5a201dc39f7a5d1a0952902a4472df14d
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-06-11 20:22:00 +0300

    Update import paths in TodoList component

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -6,7 +6,7 @@ import Divider from '@material-ui/core/Divider';
 
 import { TodosContext } from '~/context/todos.context';
 
-import Todo from '~/components/Todo';
+import Todo from '../Todo';
 
 const TodoList = () => {
   const todos = useContext(TodosContext);
